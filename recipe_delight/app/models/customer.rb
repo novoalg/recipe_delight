b79c3159992b007d1af81d35d8933e0507e0d253
Lucas Novoa <novoalg@uwec.edu>
2014-09-26 23:22:37 -0500

    Initial commit

--- recipe_delight/app/models/customer.rb
+++ recipe_delight/app/models/customer.rb
@@ -1,7 +1,14 @@
 class Customer < ActiveRecord::Base
         
     attr_accessible :email, :password, :password_confirmation, :length => { :maximum => 20 }
+    validates :email, :presence => true, :length => { :minimum => 5 }
+    validates :password, :presence => true, :length => { :minumum => 6 }
+    validates :password_confirmation, :presence => true
     has_secure_password
+
+    def self.authenticate(email, password)
+        user = find_by_email(email)
+    end
 
 
 end
